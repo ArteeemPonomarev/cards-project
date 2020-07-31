e463ccfd0c18ca05d8cac1a0ddd71fc5ec9a3ddf
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-07-31 11:47:30 +0300

    Updates

---
+++ precache-manifest.00f026ddba1049365ee37c143c3493bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "decc70b7b33b596dc585b56b94a89d84",
+    "url": "/cards-project/index.html"
+  },
+  {
+    "revision": "8de32aa1d50f5d4776d8",
+    "url": "/cards-project/static/css/main.ac4b2f45.chunk.css"
+  },
+  {
+    "revision": "785653d2967f030865ef",
+    "url": "/cards-project/static/js/2.3f969fd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards-project/static/js/2.3f969fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de32aa1d50f5d4776d8",
+    "url": "/cards-project/static/js/main.f3131e55.chunk.js"
+  },
+  {
+    "revision": "95814e0386bcbab0358c",
+    "url": "/cards-project/static/js/runtime-main.7caa687c.js"
+  }
+]);
